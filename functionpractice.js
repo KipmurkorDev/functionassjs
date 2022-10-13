ed3db3f605aa7b8d21e71030f6e86fc00605f247
EMMANUEL KIPMURKOR <56157281+KipmurkorDev@users.noreply.github.com>
2022-10-13 15:05:10 +0300

    bette functions

---
+++ functionpractice.js
@@ -0,0 +1,25 @@
+
+
+
+
+function recursive(num){
+    if(num<=100){
+   if(num%3===0 && num%5===0){
+       console.log("FizzBuzz");
+   }
+   else if(num%3===0){
+   console.log("Fizz");
+   }
+   else if(num%5===0){
+       console.log("Buzz");
+   }
+   else{
+       console.log(num);
+   }
+   // calling the function rercusively 
+   recursive(num+1)
+   }
+   }
+   // calling  the function
+   console.log(recursive(1));
+   
